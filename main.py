5560a79196017d3024d1ad273bc217dc2fbb9b67
MilianoJunior <jrmfilho37@gmail.com>
2023-07-11 21:42:49 -0300

    add second commit

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'  # Use o caminho para o seu banco de dados aqui
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:\\Users\\milianooliveira\\test.db'
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'  # Use o caminho para o seu banco de dados aqui
 db = SQLAlchemy(app)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50))
+
+with app.app_context():  # Cria um contexto de aplicação
+    # Cria o banco de dados e a tabela
+    db.create_all()
+
+    # Adiciona alguns usuários para teste
+    user1 = User(name='Alice')
+    user2 = User(name='Bob')
+
+    db.session.add(user1)
+    db.session.add(user2)
+
+    db.session.commit()
 
 @app.route('/users', methods=['GET'])
 def get_users():
